0ac0ad6e554d17f646329d584d8ebedaceb891dd
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-07-09 15:19:02 -0400

    posts are happening

--- component/Form.tsx
+++ component/Form.tsx
@@ -1,45 +1,49 @@
-import Image from 'next/image'
+import React, { useState } from 'react';
 import styles from '../styles/Home.module.css'
 import data from '../component/data.json'
 import { useForm } from 'react-hook-form'
 import axios from 'axios';
 import { users } from '../data'
 
+const api = axios.create({
+  baseURL: `http://localhost:3000/api/users`
+})
 export default function Form(props:any) {
-  const { register, handleSubmit } = useForm();
-
   const eng = props.eng
   const setReg = props.setReg
-  
-  const userDb:any = []
 
+  const { register } =useForm()
+
+  const [username, setUsername] = useState('')
+  const [inputEmail, setEmail] = useState('')
+  const [inputPassword, setPassword] = useState('')
+  // const [userDb, setUserDb] = useState([])
+  const [test, setTest] = useState('')
 
   axios.get('/api/users').then(res => {
-    console.log(res.data)
-    userDb.push(res.data)})
+    console.log("API.GET DATA:", res.data)
+    // setUserDb([res.data])
+  })
+
+  const idGenerator =()=> {
+    const num1 = Math.floor(Math.random() * 9 + 1)
+    const num2 = Math.floor(Math.random() * 9 + 1)
+
+    return `user${num1}${num2}`
+  }
       
-      const createId = () => {
-        let count = userDb.length + 1
-        return `user${count}`
+      const createUser = async () => {
+        console.log("hello")
+        console.log(username, inputEmail, inputPassword)
+        let res = axios.post('/api/users', {
+          id:idGenerator(), 
+          username: username,
+          email: inputEmail,
+          password: inputPassword
+        }).then(res => {console.log("res",res)})
+        
       }
       
-      const createUser = () => {
-        let user = {
-          id: createId(),
-          // @ts-ignores ---
-          username: document.getElementById('username').value,
-          // @ts-ignores ---
-          email: document.getElementById('email').value,
-          // @ts-ignores ---
-          password: document.getElementById('password').value
-        } 
-        userDb.push(user)
-        console.log('localUserObject', userDb)
-        axios.post('http://localhost:3000/api/users', {user}).then(res => {console.log("POSTDATA", res.data)})
-        // console.log("/API/users", users)
-      }
-      
-      const onSubmit = () => console.log(data)
 
   return (
     <div className={styles.container}>
@@ -49,7 +53,7 @@ export default function Form(props:any) {
       {eng ? data.eng.welcome : data.fr.welcome}
       </h1>
 
-      <form method='post' onSubmit={handleSubmit(onSubmit)} action="/api/users" className={styles.form}>
+      <form className={styles.form}>
         <div>
           <p className={styles.descriptionForm}>{eng ? data.eng.form : data.fr.form}</p>
 
@@ -57,37 +61,40 @@ export default function Form(props:any) {
           <input 
             type="text" 
             placeholder={eng ? data.eng.enterUser : data.fr.enterUser} 
-            // name="username" 
             id="username" 
-            {...register('data.username')}
-            required />
+            value={username}
+            onChange={event=>{setUsername(event.target.value)}}
+            required
+            />
 
           <label><b className={styles.description}>{eng ? data.eng.email : data.fr.email}</b></label>
           <input 
             type="email" 
             placeholder={eng ? data.eng.enterEmail : data.fr.enterEmail} 
-            // name="email" 
             id="email" 
-            {...register('data.email')}
-            required />
+            value={inputEmail}
+            onChange={event=>{setEmail(event.target.value)}}
+            required
+            />
 
           <label><b className={styles.description}>{eng ? data.eng.password : data.fr.password}</b></label>
           <input 
             type="password" 
             placeholder={eng ? data.eng.enterPassword : data.fr.enterPassword} 
-            // name="password"
             id="password" 
-            {...register('data.password')}
-            required />
+            value={inputPassword}
+            onChange={event=>{setPassword(event.target.value)}}
+            required
+            />
+
 
           <button type="submit" onClick={(event) => {
-            event.preventDefault
+            event.preventDefault()
+            console.log(inputEmail, inputPassword)
+            createUser()
             // @ts-ignores ---
-            if(document.getElementById('username').value && document.getElementById('email').value && document.getElementById('password').value) {
-              // createUser()
-              console.log("register", register)
               setReg(true)
-          }}} className={styles.registerbtn}><h1>{eng ? data.eng.save : data.fr.save}</h1></button>
+          }} className={styles.registerbtn}><h1>{eng ? data.eng.save : data.fr.save}</h1></button>
 
         </div>
       </form>
